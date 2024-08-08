f7574d722e4b5485b2af9ea86694e67ebef3122e
Henry <taiwoolujames@gmail.com>
2024-08-08 07:41:24 +0100

    debuggging

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginSignUp from './Components/LoginSignUp/LoginSignUp';
-import SuccessPage from './Components/LoginSignUp/successPage';
+import LoginSignUp from './Components/LoginSignUp';
+import SuccessPage from './Components/successPage';
 
 function App() {
   return (
