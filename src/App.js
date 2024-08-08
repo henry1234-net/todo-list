9e10c22a833e36cd9f77bb4e05f2a3e1603c123d
Henry <taiwoolujames@gmail.com>
2024-08-08 16:13:32 +0100

    debug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {HashRouter} from 'react-router-dom';
 import LoginSignUp from './Components/LoginSignUp';
 import SuccessPage from './Components/successPage';
 
 function App() {
   return (
-    <HashRouter>
+    <Router >
       <Routes>
         <Route path="/" Component={<LoginSignUp />} />
         <Route path="/success" Component={<SuccessPage />} />
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 
