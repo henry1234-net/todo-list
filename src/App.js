ad51b70ada34368bb36b47ea7f03aa9bd8412792
Henry <taiwoolujames@gmail.com>
2024-08-09 11:19:04 +0100

    debug

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {HashRouter} from 'react-router-dom';
 import LoginSignUp from './Components/LoginSignUp';
 import SuccessPage from './Components/successPage';
 
 function App() {
   return (
-    <Router >
+    <Router basename='https://henry1234-net.github.io/todo-list'>
       <Routes>
-        <Route path="/" Component={<LoginSignUp />} />
-        <Route path="/success" Component={<SuccessPage />} />
+        <Route path="/" element={<LoginSignUp />} />
+        <Route path="/success" element={<SuccessPage />} />
       </Routes>
     </Router>
   );
